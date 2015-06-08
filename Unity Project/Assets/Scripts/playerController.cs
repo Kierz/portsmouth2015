69769a6ca7af435039ad4e920c750c01092f80c8
Kierz <tmp06023@S7DEL125-007.uni.ds.port.ac.uk>
2015-06-08 16:32:03 +0100

    fixed constantly rotating bug when velocity equals zero

--- Unity Project/Assets/Scripts/playerController.cs
+++ Unity Project/Assets/Scripts/playerController.cs
@@ -13,7 +13,7 @@ public class playerController : MonoBehaviour
     void Start()
     {
         speed = 100.0f;
-        rotationSpeed = 45.0f;
+        rotationSpeed = 25.0f;
     }
 	
 	void FixedUpdate () 
@@ -25,15 +25,19 @@ public class playerController : MonoBehaviour
 		//Store the movement in a vector3. X, Y & Z. We don't move up on the Y axis so set to 0.
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        // store current rotation
-        Quaternion currentRotation = transform.rotation;
+        // only do this if we have a velocity
+        if (rigidbody.velocity.magnitude > 0.01f)
+        {
+            // store current rotation
+            Quaternion currentRotation = transform.rotation;
 
-        // point object at intended facing direction
-        transform.LookAt(new Vector3(transform.position.x + rigidbody.velocity.x, transform.position.y, transform.position.z + rigidbody.velocity.z));
+            // point object at intended facing direction
+            transform.LookAt(new Vector3(transform.position.x + rigidbody.velocity.x, transform.position.y, transform.position.z + rigidbody.velocity.z));
 
-        // lerp from current rotation to intended facing direction
-        transform.rotation = Quaternion.Lerp(currentRotation, transform.rotation, Time.deltaTime * rotationSpeed);
-	
+            // lerp from current rotation to intended facing direction
+            transform.rotation = Quaternion.Lerp(currentRotation, transform.rotation, Time.deltaTime * rotationSpeed);
+        }
+
 		//Add the force to the rigid body, depending on the speed.
 		rigidbody.AddForce(movement * speed);
 	}
