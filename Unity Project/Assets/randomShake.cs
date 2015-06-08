8609f035a067ea5796b54ca67cccce912e41a7f9
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-08 15:03:52 +0100

    philShakeTemplate

---
+++ Unity Project/Assets/randomShake.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomShake : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        transform.localPosition = Random.insideUnitSphere * 0.1f/*shakeAmount*/;
+
+    }
+}
+	
+	
+
