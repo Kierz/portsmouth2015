952a6645e86eb54d25b396662095f7060aef1e37
unknown <tmp06022@S7EL125-009.uni.ds.port.ac.uk>
2015-06-08 16:17:50 +0100

    Stashing

--- Unity Project/Assets/Scripts/playerController.cs
+++ Unity Project/Assets/Scripts/playerController.cs
@@ -25,11 +25,34 @@ public class playerController : MonoBehaviour {
 		//Store the movement in a vector3. X, Y & Z. We don't move up on the Y axis so set to 0.
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
+		print ("Test 1 " + transform.rotation);
+
 		transform.forward = rigidbody.velocity;
 
-
+		print ("Test 2 " + transform.rotation);
 	
 		//Add the force to the rigid body, depending on the speed.
 		rb.AddForce (movement * speed);
+
+		//Lerp so that the player does not rotate too quickly
+		//Vector3.Lerp(movement, Vector3, float rate): Vector3;
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Slow") 
+		{
+			//Halve speed
+		}
+
+		/*if (collision.gameObject.tag == "Kill") 
+		{
+			//Kill bat + game over
+		}
+
+		if (collision.gameObject.tag == "") 
+		{
+			
+		}*/
 	}
 }
