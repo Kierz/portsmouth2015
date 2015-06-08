4a764f5a4d9cd03294c20327f275992458c4c430
unknown <tmp06023@S7DEL125-007.uni.ds.port.ac.uk>
2015-06-08 15:03:34 +0100

    stashing

---
+++ Unity Project/Assets/Code/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    private playerController player;
+
+    void Start()
+    {
+        player = FindObjectOfType<playerController>();
+    }
+
+    void Update ()
+    {
+        transform.position = player.transform.position;
+	}
+}
