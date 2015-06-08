72b28653b2a1ea937bb1e37a5b7d9a40562bb20d
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-08 16:23:46 +0100

    philShakeTemplate

--- Unity Project/Assets/randomShake.cs
+++ Unity Project/Assets/randomShake.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class randomShake : MonoBehaviour
+public class RandomShake : MonoBehaviour
 {
+    private Vector3 newPos;
+    float triggerDistance = 0.01f;
+     
 
+   /* private static Vector3 Vector3(int p1, int p2, int p3)
+    {
+        throw new System.NotImplementedException();
+    }*/
     // Use this for initialization
     void Start()
     {
@@ -13,8 +20,23 @@ public class randomShake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-        transform.localPosition = Random.insideUnitSphere * 0.1f/*shakeAmount*/;
+        newPos = Random.insideUnitSphere;
+
+        float distanceBetweenPoints = (transform.position - newPos).magnitude;
+        if (distanceBetweenPoints <= triggerDistance)
+        {
+            newPos = Random.insideUnitSphere;
+        }
+
+
+
+
+
+
+
+
+
+       // transform.localPosition = Random.insideUnitSphere * 0.1f/*shakeAmount*/;
 
     }
 }
