749409eb7b7a974eb31f55611ddd5a7769bcf1d0
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-08 16:47:19 +0100

    initial shake script

--- Unity Project/Assets/randomShake.cs
+++ Unity Project/Assets/randomShake.cs
@@ -14,18 +14,18 @@ public class RandomShake : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        newPos = Random.insideUnitSphere;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // newPos = Random.insideUnitSphere;
+        
 
         float distanceBetweenPoints = (transform.position - newPos).magnitude;
         if (distanceBetweenPoints <= triggerDistance)
         {
-            newPos = transform.position - Random.insideUnitSphere;
+            newPos = Random.insideUnitSphere;
         }
 
 
