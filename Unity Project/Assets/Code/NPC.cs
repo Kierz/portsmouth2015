65f2d334d24487554b0640d9adc3c31772793a04
hyriar <james_davidson@me.com>
2015-06-09 15:35:59 +0100

    Stashing

--- Unity Project/Assets/Code/NPC.cs
+++ Unity Project/Assets/Code/NPC.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class NPC : Character
 {
-	void Start () 
+    private int health, speed, xpos, ypos;
+    bool isDead;
+
+    public GameManager gm;
+
+	void Start() 
     {
-	
+        isDead = false;
+        health = 1;
+        xpos = Random.RandomRange();
 	}
 	
-    void Update () 
+    void Update() 
     {
-	
+        if (isDead)
+        {
+            break;
+        }
+
+	    if(health <= 0)
+        {
+            explode();
+            isDead = true;
+        }
 	}
+
+    void explode()
+    {
+
+    }
 }
