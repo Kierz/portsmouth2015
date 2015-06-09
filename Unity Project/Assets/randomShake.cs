61cbbdf715300fa76df6f9faef245b08b03ecd74
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-09 10:55:38 +0100

    wobble script

--- Unity Project/Assets/randomShake.cs
+++ Unity Project/Assets/randomShake.cs
@@ -6,12 +6,6 @@ public class RandomShake : MonoBehaviour
     private Vector3 newPos;
     float triggerDistance = 0.01f;
      
-
-   /* private static Vector3 Vector3(int p1, int p2, int p3)
-    {
-        throw new System.NotImplementedException();
-    }*/
-    // Use this for initialization
     void Start()
     {
         newPos = Random.insideUnitSphere;
@@ -19,25 +13,17 @@ public class RandomShake : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
 
         float distanceBetweenPoints = (transform.position - newPos).magnitude;
+
         if (distanceBetweenPoints <= triggerDistance)
         {
-            newPos = Random.insideUnitSphere;
+            newPos = Random.insideUnitSphere*0.5f;
         }
 
-
-
-
-
-
-
-
-
-       // transform.localPosition = Random.insideUnitSphere * 0.1f/*shakeAmount*/;
-
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 30);
+        
     }
 }
 	
