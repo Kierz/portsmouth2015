cc62717a2f1edb07f2bb6985e8fa80abe4c0c16a
Kierz <kierz@users.noreply.github.com>
2015-06-11 21:45:49 +0100

    Remade sound files as mp3

---
+++ Unused Assets/MainMenuObj.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+	//Detect when mouse clicks on a button
+	void OnMouseDown()
+	{
+		if (name == "Start Game") {
+			Application.LoadLevel("Main");
+		}
+		
+		if (name == "Exit Game") {
+			Application.Quit();
+		}
+	}
+}
