6e55435e5b6d64498897f1d3e6410d990b7f4ab6
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-11 09:54:15 +0100

    Lives indicator

--- Unity Project/Assets/Code/HudStuff.cs
+++ Unity Project/Assets/Code/HudStuff.cs
@@ -10,18 +10,24 @@ public class HudStuff : MonoBehaviour
     public GUIText guiLives;
     public GUIText playerNumber;
     public GUIText pressStart;
-      
-    public Player player;
 
+    public SpriteRenderer Life1;
+    public SpriteRenderer Life2;
+    public SpriteRenderer Life3;
+
+    public Player player;
+    private int livesRemaining;
     private int score = 0;    
  
     private bool blinking = true;
 	
 	void Start () 
     {
-        guiScore.text = "Score: 0";
-        guiLives.text = "Lives: 3";
-       
+        //guiScore.text = "Score: 0";
+        //guiLives.text = "Lives: ";
+        Life1.renderer.enabled = false;
+        Life2.renderer.enabled = false;
+        Life3.renderer.enabled = false;
         StartCoroutine(Blink());
        
 	}
@@ -35,11 +41,31 @@ public class HudStuff : MonoBehaviour
         
         if (player.IsActive())
         {
-            guiScore.text = "Score: " + player.GetScore();
-            guiLives.text = "Lives: " + player.GetLives();
+            guiScore.text = "Score: " +player.GetScore();
+            guiLives.text = "Lives: " +player.GetLives();
             playerNumber.text = "Player ";
             blinking = false;//turn off blinking
-           
+            livesRemaining = player.GetLives();
+            if (livesRemaining == 3)
+            {
+                Life1.renderer.enabled = true;
+                Life2.renderer.enabled = true;
+                Life3.renderer.enabled = true;
+            }
+            else
+                if (livesRemaining ==2)
+                {
+                    Life1.renderer.enabled = true;
+                    Life2.renderer.enabled = true;
+                    Life3.renderer.enabled = false;
+                }
+                else
+                    if (livesRemaining == 1)
+                    {
+                        Life1.renderer.enabled = true;
+                        Life2.renderer.enabled = false;
+                        Life3.renderer.enabled = false;
+                    }//put death conditions below
         }
 
     }
