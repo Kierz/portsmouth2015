642f0ed56af05ab5c864d5d33db0f73409dfd82c
KingFilXIVII <KingFilXIVII@users.noreply.github.com>
2015-06-11 12:44:01 +0100

    player boundaries

--- Unity Project/Assets/Code/Player.cs
+++ Unity Project/Assets/Code/Player.cs
@@ -166,10 +166,34 @@ public class Player : Character
 
 		Vector3 newPosition = transform.position + ( movement * speed * speedFactor * Time.deltaTime );
 
-		//if ( !IsOffScreen( newPosition ) )		this was awfull, dont use it!
+		/*if ( !IsOffScreen( newPosition ) )		this was awfull, dont use it!
 		{
 			transform.position = newPosition;
-		}
+		}*/
+
+
+        // X axis
+        if (transform.position.x <= -3.6f)
+        {
+            transform.position = new Vector3(-3.6f, transform.position.y,transform.position.z);
+        }
+        else if (transform.position.x >= 3.6f)
+        {
+            transform.position = new Vector3(3.6f, transform.position.y,transform.position.z);
+        }
+
+        // Z axis
+        if (transform.position.z <= -2.7f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,-2.7f);
+        }
+        else if (transform.position.z >= 2.7f)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y, 2.7f);
+        }
+
+
+
 	}
 
 	private void Respawn()
